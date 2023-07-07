d824e079e101da4b2d7c76df4755d05cf3d4df07
gbischof <bisc8233@gmail.com>
2023-07-06 20:55:00 -0400

    motor is working

---
+++ bluesky_unreal/examples/unreal_motor.py
@@ -0,0 +1,20 @@
+"""
+How to use an UnrealSignal.
+
+tests/api.py can be used to simulate the unreal remote control api.
+TODO: Change this to markdown.
+"""
+
+from bluesky import RunEngine
+from bluesky_unreal import UnrealMotor
+
+TEST_SERVER='http://localhost:8000'
+
+# Create a motor.
+motor = UnrealMotor('dcm:bragg', server=TEST_SERVER)
+
+
+## Use the device in a plan.
+#RE = RunEngine()
+#RE.subscribe(print)
+#RE(count([device1.bragg], num=3, delay=1))
--- bluesky_unreal/motor.py
+++ bluesky_unreal/motor.py
@@ -2,22 +2,24 @@ from ophyd import FormattedComponent, Device
 from ophyd.positioner import SoftPositioner
 from bluesky_unreal import UnrealClient, UnrealSignal
 
+TEST_SERVER='http://localhost:8000'
+
+
 class UnrealMotor(Device, SoftPositioner):
     """
     An Unreal Engine ophyd motor which tries to mimic some of the functionality of EpicsMotor.
     """
 
     # FormattedComponent doesn't prefix the parent name.
-    user_readback = FormattedComponent(UnrealSignal, 'dcm:bragg')
-    user_setpoint = FormattedComponent(UnrealSignal, 'dcm:bragg')
+    user_readback = FormattedComponent(UnrealSignal, '{self.name}', server=TEST_SERVER)
+    user_setpoint = FormattedComponent(UnrealSignal, '{self.name}', server=TEST_SERVER)
 
-    def __init__(self, prefix, **kwargs):
-        super().__init__(prefix, **kwargs)
-        server_address='http://localhost:30010'
-        self._client = UnrealClient(server_address)
-        self.user_readback.name = prefix
-        self.user_setpoint.name = prefix
-        print("NAME", self.name, "PREFIX", self.prefix)
+    def __init__(self, variable_name, *, name=None, server='http://localhost:30010', **kwargs):
+        name=variable_name
+        self.server = server
+        super().__init__(name=name, **kwargs)
+        self._client = UnrealClient(server)
+
         try:
             init_pos = self._client.get_value(f"{self.name}")
             self._position= init_pos
