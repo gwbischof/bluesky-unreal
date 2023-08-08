3a40191544b750223036833889f8cb3a67148ecf
mguercio626 <mguercio626@gmail.com>
2023-08-08 09:10:25 -0400

    changes invloving mirrors

--- bluesky_unreal/motor.py
+++ bluesky_unreal/motor.py
@@ -1,4 +1,4 @@
-from ophyd import FormattedComponent, Device
+from ophyd import FormattedComponent, Device, Signal
 from ophyd.positioner import SoftPositioner
 from ophyd.sim import SynSignal
 from bluesky_unreal import UnrealClient, UnrealSignal
@@ -7,6 +7,11 @@ from bluesky_unreal import UnrealClient, UnrealSignal
 #SERVER='http://localhost:8000'
 SERVER='http://localhost:30010'
 
+
+class EnumSignal(Signal):
+    def enum_strs(self):
+        return ("Off","On")
+    
 
 class UnrealMotor(Device, SoftPositioner):
     """
@@ -25,17 +30,24 @@ class UnrealMotor(Device, SoftPositioner):
     """
 
     # FormattedComponent doesn't prefix the parent name.
-    user_readback = FormattedComponent(UnrealSignal, '{self.name}', server=SERVER, kind='normal')
-    user_setpoint = FormattedComponent(UnrealSignal, '{self.name}', server=SERVER, kind='normal')
-    user_offset = FormattedComponent(SynSignal, func=lambda: 1, kind='normal')
+    user_readback = FormattedComponent(UnrealSignal, '{self.variable_name}', server=SERVER, kind='normal')
+    user_setpoint = FormattedComponent(UnrealSignal, '{self.variable_name}', server=SERVER, kind='normal')
+    user_offset = FormattedComponent(SynSignal, func=lambda: 1., kind='config')
+    velocity = FormattedComponent(Signal, value=1., kind='config')
+    hocpl = FormattedComponent(Signal, value= 1., kind='config')
+    amfe =  FormattedComponent(EnumSignal, value= 0, kind='config')
+    amfae =  FormattedComponent(EnumSignal, value= 0, kind='config')
+
 
     def __init__(self, variable_name, *, name=None, server='http://localhost:30010', **kwargs):
-        name=variable_name
+        #name=variable_name
+        self.variable_name = variable_name
         self.server = server
         self._client = UnrealClient(server)
-        init_pos = self._client.get_value(f"{name}")
+        init_pos = self._client.get_value(variable_name)
         super().__init__(name=name, init_pos=init_pos, **kwargs)
 
     def _setup_move(self, position, status):
-         self._client.set_value(f"{self.name}", position)
+         self._client.set_value(f"{self.variable_name}", position)
          super()._setup_move(position, status)
+
